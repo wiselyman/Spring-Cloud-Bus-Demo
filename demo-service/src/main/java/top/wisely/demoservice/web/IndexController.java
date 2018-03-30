9149dd672dc1c19b70ab81ab32fcb430fce5fe21
wangyunfei <wiselyman2008@gmail.com>
2018-03-30 18:38:15 +0800

    xx

---
+++ demo-service/src/main/java/top/wisely/demoservice/web/IndexController.java
@@ -0,0 +1,16 @@
+package top.wisely.demoservice.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @Value("author.name")
+    String authorName;
+
+    @GetMapping("/")
+    public String index(){
+        return authorName;
+    }
+}

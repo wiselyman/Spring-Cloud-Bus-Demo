37edd011003b5d0ed22da24857e30b87b5c65cba
wangyunfei <wiselyman2008@gmail.com>
2018-03-30 18:02:01 +0800

    first commit

---
+++ cloudbus/src/main/java/top/wisely/cloudbus/CloudbusApplication.java
@@ -0,0 +1,12 @@
+package top.wisely.cloudbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbusApplication.class, args);
+	}
+}
---
+++ cloudbus/src/test/java/top/wisely/cloudbus/CloudbusApplicationTests.java
@@ -0,0 +1,16 @@
+package top.wisely.cloudbus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudbusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

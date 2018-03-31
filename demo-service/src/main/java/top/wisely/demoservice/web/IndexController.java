2e56ccf9576985242f5f62933b12761da00fcecd
wangyunfei <wiselyman2008@gmail.com>
2018-03-31 20:46:21 +0800

    x

--- demo-service/src/main/java/top/wisely/demoservice/web/IndexController.java
+++ demo-service/src/main/java/top/wisely/demoservice/web/IndexController.java
@@ -1,10 +1,12 @@
 package top.wisely.demoservice.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class IndexController {
     @Value("${author.name}")
     String authorName;

c521a21fadee9d6afceb4989ac6968609ac1152f
wangyunfei <wiselyman2008@gmail.com>
2018-03-31 17:09:17 +0800

    xx

--- demo-service/src/main/java/top/wisely/demoservice/web/IndexController.java
+++ demo-service/src/main/java/top/wisely/demoservice/web/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    @Value("author.name")
+    @Value("${author.name}")
     String authorName;
 
     @GetMapping("/")
